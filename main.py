7d57df4a01eb4e4f0cb1f82a4e88f10eb1db76b6
ilushandr <ilushandr@gmail.com>
2021-02-06 19:06:57 +0500

    init 3 task

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import requests
 
 KEYS = (pygame.KEYDOWN, pygame.K_PAGEUP, pygame.K_UP, pygame.K_DOWN,
         pygame.K_LEFT, pygame.K_RIGHT)
-url_static = 'http://static-maps.yandex.ru/1.x/'
+url_static = 'https://static-maps.yandex.ru/1.x/'
+w, h = size = 650, 450
+delta = 180
 
 
 class Map:
-    def __init__(self, coord, zoom, layer='map', size=(650, 450)):
+    def __init__(self, coord, zoom, layer='map', size=size):
         self.lon, self.lat = coord
         self.z = zoom
         self.layer = layer
         self.w, self.h = size
+        self.map = None
         self.update_map()
 
     def update_map(self):
@@ -28,10 +31,20 @@ class Map:
         self.map = pygame.image.load(BytesIO(response.content))
 
     def update(self, event):
+        lon_delta = delta / (2 ** self.z)
+        lat_delta = delta / (2 ** self.z) / 2
         if event.key == pygame.K_PAGEDOWN:
             self.z = max(0, self.z - 1)
         elif event.key == pygame.K_PAGEUP:
             self.z = min(20, self.z + 1)
+        elif event.key == pygame.K_LEFT:
+            self.lon = (self.lon + 180 - lon_delta) % 360 - 180
+        elif event.key == pygame.K_RIGHT:
+            self.lon = (self.lon + 180 + lon_delta) % 360 - 180
+        elif event.key == pygame.K_UP and abs(self.lat + lat_delta) < 85:
+            self.lat += lat_delta
+        elif event.key == pygame.K_DOWN and abs(self.lat - lat_delta) < 85:
+            self.lat -= lat_delta
         if event.key in KEYS:
             mapapp.update_map()
 
@@ -39,7 +52,7 @@ class Map:
 pygame.init()
 screen = pygame.display.set_mode((650, 450))
 coord = (38.2052612, 44.4192543)
-z = 17
+z = 0
 mapapp = Map(coord, z)
 
 running = True
@@ -52,4 +65,3 @@ while running:
 
     screen.blit(mapapp.map, (0, 0))
     pygame.display.flip()
-pygame.quit()
